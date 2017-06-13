bf4e7823efaab3fc199637b54c5b437393fede05
Larry Cessna <larry.cessna@outlook.com>
2017-06-12 18:17:18 -0700

    started on html, the table is still not working, but printing something

--- app.js
+++ app.js
@@ -1,114 +1,91 @@
 'use strict';
 //
 
-var locationsObject = {
-  0: 'First and Pike',
-  1: 'SeaTac Airport',
-  2: 'Seattle Center',
-  3: 'Capitol Hill',
-  4: 'Alki',
-};
-
-var firstAndPike = {
-  location: '1st and Pike',
-  minHourlyCust: '23',
-  maxHourlyCust: '65',
-  avgPerCust: '6.3',
-  workingHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
-};
-
-var seaTacAirport = {
-  location : 'SeaTac Airport',
-  minHourlyCust : '3',
-  maxHourlyCust: '24',
-  avgPerCust: '1.2',
-  workingHours : ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
-};
-
-var seattleCenter = {
-  location: 'Seattle Center',
-  minHourlyCust: '11',
-  maxHourlyCust: '38',
-  avgPerCust: '3.7',
-  workingHours : ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
-};
-
-var capitolHill = {
-  location: 'Captiol Hill',
-  minHourlyCust: '20',
-  maxHourlyCust: '38',
-  avgPerCust: '2.3',
-  workingHours : ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
-};
-
-var alki = {
-  location: 'Alki',
-  minHourlyCust: '2',
-  maxHourlyCust: '16',
-  avgPerCust: '4.6',
-  workingHours : ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
-};
-
-var hourlySales = function(minHourlyCust, maxHourlyCust, avgPerCust) {
-  this.minHourlyCust = minHourlyCust;
-  this.maxHourlyCust = maxHourlyCust;
-  this.avgPerCust = avgPerCust;
-};
-
-hourlySales.generateRandom = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function customerNumber(store) {
+  return Math.floor(Math.random() * ((store.maxCustHour + 1) - store.minCustHour) + store.minCustHour);
 }
 
-hourlySales.generateRandom(minHourlyCust, maxHourlyCust);
-//console.log(.generateRandom(1, 5));
-console.log(hourlySales.generateRandom(1, 5));
-
-
-locations.minHourlyCust = function(){
-  console.log('Minimum customers per hour at ' + locationName + ' is ' + minHourlyCust + '.');
-};
-
-//
-// user.login = function(){
-//   console.log('The user has logged in!!!');
-// };
-//
-// user.logOut = function(){
-//   console.log('The user has logged out...');
-// };
-//
-// console.log('Users Info - first name: ' + user.firstName);
-// console.log('Users Info - last name: ' + user.lastName);
-// console.log('Users Info - email: ' + user.email);
-// console.log('Users Info - age: ' + user.age);
-// console.log('Users Info - pets: ' + user.pets);
-//
-// user.hasCuteDog = true;
-// // user.fullName = 'Adam Wallraff';
-// user.firstName = 'Bob';
-//
-// user.fullName = user.firstName + ' ' + user.lastName;
-//
-//
-// console.log('Users Info - full name: ' + user.fullName);
-//
-//
-// console.log('Users Info - is their dog cute?: ' + user.hasCuteDog);
-//
-// console.log('USER: ' + user);
-// console.log(user);
-//
-// user.login();
-
-
-var ulElement = document.getElementById('primary-list');
-console.log(ulElement);
-
-for (var i = 0; i < 1000; i++) {
-  var listItemElement = document.createElement('li');
-
-  listItemElement.textContent = 'This is item ' + i;
-
-  ulElement.appendChild(listItemElement);
-  console.log(ulElement.children);
+function cookieCountAll() {
+  for(var i = 0; i < allStores.length; i++) {
+    cookieCount(allStores[i]);
+  }
 }
+
+function cookieCount(store) {
+  cookiesPerHour(store);
+  cookiesTotal(store);
+  printSales(store);
+}
+
+function cookiesPerHour(store) {
+  for (var i = 0; i < store.hoursOpen.length; i++) {
+    store.cookiesPerHour.push(Math.floor(store.avgCustConsumed * customerNumber(store)));
+  }
+}
+
+function cookiesTotal(store) {
+  var totalCookies = 0;
+  for(var i = 0; i < store.hoursOpen.length; i++) {
+    totalCookies = totalCookies + store.cookiesPerHour[i];
+    console.log('Total cookies = ' + totalCookies);
+    //console.log(store.cookiesPerHour[i] + ' per hour cause I want to see them.');
+  }
+  store.totalCookies = totalCookies;
+  console.log(store.totalCookies);
+}
+
+function printSales(store){
+  //Step 1 - Create Element
+  var newRowEl = document.createElement('generated-table');
+
+  //Step 2 - Manipulate the data
+  for(var i = 0; i < store.hoursOpen.length; i++) {
+    var hoursOpenEl = document.createElement('th');
+    hoursOpenEl.textContent = store.hoursOpen[i];
+    newRowEl.appendChild(hoursOpenEl);
+  }
+
+  var storeLocationEl = document.createElement('td');
+  storeLocationEl.textContent = store.location;
+  newRowEl.appendChild(storeLocationEl);
+
+  var cookiesPerHourEl = document.createElement('td');
+  cookiesPerHourEl.textContent = store.cookiesPerHour;
+  newRowEl.appendChild(cookiesPerHourEl);
+
+  var totalCookiesPerHourEl = document.createElement('td');
+  totalCookiesPerHourEl.textContent = store.totalCookies[i];
+  newRowEl.appendChild(totalCookiesPerHourEl);
+
+  //append to the dom
+  tableBodyEl.appendChild(newRowEl);
+
+}
+
+function Store(location, minCustHour, maxCustHour, averageCustomerCookies) {
+  this.location = location;
+  this.minCustHour = minCustHour;
+  this.maxCustHour = maxCustHour;
+  this.avgCustConsumed = averageCustomerCookies;
+  this.cookiesPerHour = [],
+  this.totalCookies = 0,
+  this.hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+}
+
+var firstAndPikeStore = new Store('1st and Pike', 23, 65, 6.3);
+var seaTacStore = new Store('SeaTac Airport', 3, 24, 1.2);
+var seaCenterStore = new Store('Seattle Center', 11, 38, 3.7);
+var capitolHillStore = new Store('Capitol Hill', 20, 38, 2.3);
+var alkiStore = new Store('Alki', 2, 16, 4.6);
+
+var allStores = [firstAndPikeStore, seaTacStore, seaCenterStore, capitolHillStore, alkiStore];
+
+var tableEl = document.getElementById('generated-table');
+var tableBodyEl = document.createElement('tbody');
+tableEl.appendChild(tableBodyEl);
+
+for (var i = 0; i < allStores.length; i++) {
+  printSales(allStores[i]);
+}
+
+cookieCountAll();
